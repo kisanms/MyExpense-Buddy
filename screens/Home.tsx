f1d11e7ed5eb85af283d8e50385ceb0b55c1d986
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-02-04 17:15:06 +0530

    data from db with beautiful ui

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Category, Transaction } from "../types";
+import { useSQLiteContext } from "expo-sqlite";
+import TransactionList from "../components/TransactionsList";
 
 const Home = () => {
+  const [categories, setCategories] = React.useState<Category[]>([]);
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+
+  const db = useSQLiteContext();
+
+  useEffect(() => {
+    db.withTransactionAsync(async () => {
+      await getData();
+    });
+  }, [db]);
+
+  async function getData() {
+    const result = await db.getAllAsync<Transaction>(
+      `SELECT * FROM Transactions ORDER BY date DESC`
+    );
+    setTransactions(result);
+
+    const categoriesResult = await db.getAllAsync<Category>(
+      `SELECT * FROM Categories;`
+    );
+    setCategories(categoriesResult);
+  }
+
+  async function deleteTransaction(id: number) {
+    db.withTransactionAsync(async () => {
+      await db.runAsync(`DELETE FROM Transactions WHERE id = ?;`, [id]);
+      await getData();
+    });
+  }
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ padding: 15, paddingVertical: 165 }}>
+      <TransactionList
+        categories={categories}
+        transactions={transactions}
+        deleteTransaction={deleteTransaction}
+      />
+    </ScrollView>
   );
 };
 
